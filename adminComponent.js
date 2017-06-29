2c4f3d4c404d85fe472cf576fd39a50131cc4451
sebtn <sebtn@users.noreply.github.com>
2017-06-29 16:37:07 -0400

    Update component

--- adminComponent.js
+++ adminComponent.js
@@ -1,8 +1,8 @@
 import React, {component} from 'react'
 
-let isAdmin = true
+let isAdmin = false
 let adminComponent = (Component) => {
-  return class Admin extends Component {
+  return class Admin extends React.Component {
     render() {
       if(isAdmin) {
         return (
@@ -17,8 +17,36 @@ let adminComponent = (Component) => {
     }
   }
 }
+/*Using component extension, can alter component's life cycle
+and render methods */
+let isAdmin = true
+let adminComponent = (Component) => {
+  return class Admin extends Component {
+    componentDidUpdate =() => {
+      console.log('Admin did update')
+      if (super.componentDidUpdate) {
+        super.componentDidUpdate
+      }
+    })
+    render() {
+      if(isAdmin) {
+        return (
+          <div>
+            <p>Private admin info</p>
+            {super.render()} 
+          </div>
+        )
+      } else {
+        return null
+      }
+    }
+  }
+}
 
 class ComponentTwo extends Component {
+  componentDidUpdate =() => {
+    console.log('ComponentTwo did update')
+  })
   constructor(props) {
     super(props)
     this.state = {
