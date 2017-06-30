59f5cb6c09b8bbb66eeef5d3af0d6a9d4fd9a0b2
sebtn <sebtn@users.noreply.github.com>
2017-06-29 23:51:32 -0400

    Update notes on redux

--- NotesOnRedux.js
+++ NotesOnRedux.js
@@ -1,3 +1,24 @@
+//main app
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import  '././styles/app.scss' 
+import BookApp from './components/book-app'
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)} >
+		<BookApp />
+	</Provider> 
+  ,document.getElementById('root'))
+
+//--------------------------------------------------------------
+
 // inside a reducer file 
 
 /* state argument is not the apps's state, 
