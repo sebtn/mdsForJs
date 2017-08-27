2c037ffa79a68e81e03b1406179036f180446051
sebtn <sebtn@users.noreply.github.com>
2017-08-27 00:28:51 -0400

    Update Observables.js

--- observable.js
+++ observable.js
@@ -16,8 +16,9 @@ error() = > {...},
 })
 */
 
+/*----------------------------------------------------------------*/
 var observer =  {
-  next: (value) => console.log(value),
+  next: (value) => console.log('next(): ', value),
   error: (error) => console.log(error),
   complete: () => console.log('Completed') // will never be called
 }
@@ -26,6 +27,7 @@ Rx.Observable.fromEvent(button, 'click')
     (value) => console.log(value.client()) //observer
   )
 
+/*----------------------------------------------------------------*/
 Rx.Observable.fromEvent(button, 'click')
   .subscribe(observer)
 
@@ -37,6 +39,7 @@ Rx.Observable.create((obs) => {
 }) 
   .subscribe(observer)
 
+/*----------------------------------------------------------------*/
 // async
 Rx.Observable.create((obs) => {
   obs.next('A val')
@@ -55,3 +58,53 @@ Rx.Observable.create((obs) => {
   button.onclick = (event) => obs.next(event) // instead of fromEvent
 }) 
   .subscribe(observer)
+
+/*----------------------------------------------------------------*/
+// Observable is a time dimension so debounce, thtottle, 
+// buffer and combineLast become trivial... are lazy also repeat()
+
+// An epic usin rxJs redux to manipulate 
+//  state 
+const pingPongEpic = (action$, store) =>
+  action$.ofType('PING')
+    .map(action => ( {type: 'PONG'}) )
+
+/*----------------------------------------------------------------*/
+const pingPongEpic = (action$, store) =>
+  action$.ofType('PING')
+    .delay(1000) // built in operator
+    .map(action => ( {type: 'PONG'}) )
+
+  // using the reducer
+  const isPing = (state = false, action) => {
+    switch(action.type) {
+      case "PING":
+        return true
+      case "PONG":
+        return false
+      default:
+        return state
+    }
+  }
+  
+/*----------------------------------------------------------------*/
+  const counter = (state = 0, action) => {
+    switch(action.type) {
+      case "INCREMENT":
+        return state + 1
+      case "DECREMENT":
+        return state - 1 
+      default:
+        return state
+    }
+  }
+// wait 1 second to update
+const incrementEpic = (action$, store) => 
+  action$.ofType('INCREMENT_DEBOUNCED')
+    .debounceTime(500)
+    .map(() =>( {type: 'INCREMENT'} )) 
+
+const incrementEpic = (action$, store) => 
+  action$.ofType('DECREMENT_DEBOUNCED')
+    .debounceTime(500)
+    .map(() =>( {type: 'DECREMENT'} ))
