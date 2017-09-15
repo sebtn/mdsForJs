a477ea4531bead8936a6073663865794920e4d21
sebtn <sebtn@users.noreply.github.com>
2017-09-14 21:59:48 -0400

    minor edit

--- router.js
+++ router.js
@@ -56,11 +56,11 @@ export default AboutPage
 //------------------------------------------
 import toRegex from 'path-to-regexp'
 function matchURI(path, uri) {
-  const keys = [];
+  const keys = []
   const pattern = toRegex(path, keys); // TODO: Use caching
-  const match = pattern.exec(uri);
-  if (!match) return null;
-  const params = Object.create(null);
+  const match = pattern.exec(uri)
+  if (!match) return null
+  const params = Object.create(null)
   for (let i = 1; i < match.length; i++) {
     params[keys[i - 1].name] =
       match[i] !== undefined ? match[i] : undefined;
