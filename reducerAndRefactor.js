13172d95f5abf7bc31b07b208c73d5b9f2b68a03
sebtn <sebtn@users.noreply.github.com>
2017-10-06 19:03:47 -0400

    Update file

--- reducerAndRefactor.js
+++ reducerAndRefactor.js
@@ -125,3 +125,111 @@ export default function artistsReducer(state=initialState, action) {
 //       return state 
 //     }
 // }
+
+
+//-----------------------------------------------------------------
+//authReducer
+import { List, Map, fromJS } from 'immutable'
+import {
+  SPOTIFY_TOKENS, 
+  SPOTIFY_ME_BEGIN, 
+  SPOTIFY_ME_SUCCESS, 
+  SPOTIFY_ME_FAILURE
+} from '../actions'
+
+// /** The initial state; no tokens and no user info */
+// con
+
+const initialState = {
+  accessToken: null,
+  refreshToken: null,
+  auth: {
+    loading: false,
+    country: null,
+    display_name: null,
+    email: null,
+    external_urls: {},
+    followers: {},
+    href: null,
+    id: null,
+    images: [],
+    product: null,
+    type: null,
+    uri: null,
+  }
+}
+
+// function spotifyTokens(state) {
+//   var newState = Map({
+//     refreshToken,
+//     accessToken
+//   })
+//   return state.merge(newState)
+// }
+
+// function spotifyMeBegin(state, auth) {
+//   var newState = fromJS({
+//     auth, 
+//     loading: true
+//   })
+//   return state.merge(newState)  
+// }
+
+// function spotifyMeSuccess(state, auth) {
+//   var newState = fromJS({
+//     auth, 
+//     data,
+//     loading: false
+//   })
+//   return state.merge(newState)  
+// }
+
+// /* Auth reducer */
+// export default function reduce(state = initialState, action) {
+//   switch (action.type) {
+
+//   //set the token
+//   case SPOTIFY_TOKENS:
+//     return spotifyTokens(state, action.accessToken, action.refreshToken)
+
+//   case SPOTIFY_ME_BEGIN:
+//     return spotifyMeBegin(state, action.loading) 
+
+//   case SPOTIFY_ME_SUCCESS:
+//     return spotifyMeSuccess(state, action.data, action.loading)
+
+//   case SPOTIFY_ME_FAILURE:
+//     return state
+
+//   default:
+//     return state
+//   }
+// }
+
+export default function reduce(state = initialState, action) {
+  switch (action.type) {
+  // when we get the tokens... set the tokens!
+  case SPOTIFY_TOKENS:
+    const {accessToken, refreshToken} = action;
+    return Object.assign({}, state, {accessToken, refreshToken});
+
+  // set our loading property when the loading begins
+  case SPOTIFY_ME_BEGIN:
+    return Object.assign({}, state, {
+      auth: Object.assign({}, state.auth, {loading: true})
+    });
+
+  // when we get the data merge it in
+  case SPOTIFY_ME_SUCCESS:
+    return Object.assign({}, state, {
+      auth: Object.assign({}, state.auth, action.data, {loading: false})
+    });
+
+  // currently no failure state :(
+  case SPOTIFY_ME_FAILURE:
+    return state;
+
+  default:
+    return state;
+  }
+}
