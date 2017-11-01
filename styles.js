f38b59d04a254c9c61a0ca8c9ea3aeb633a019d0
sebtn <sebtn@users.noreply.github.com>
2017-11-01 14:02:31 -0400

    update to styles.js file

--- styles.js
+++ styles.js
@@ -277,3 +277,184 @@ List([2, 4, 6])
   .filter(num => num > 5)
   .fold(log)
 //=> [8, 12]
+                        
+                       
+// try noi udefined and no null                        
+const getBody = document => document && document.body ? document.body : undefined
+const getBody = document => document && document.body
+
+// good, a monadic api is more reliable and defers the decisions to the caller
+const getBody = document =>
+  Either.fromNullable(document)
+  .map(d => d.body)
+
+// good
+const getBody = document =>
+  Either.fromNullable(document)
+  .map(d => d.body)
+
+// Basic usage
+Future((reject, resolve) => resolve('Yay'))
+  .map(res => res.toUpperString())
+  .fork(
+    err => log(`Err: ${err}`),
+    res => log(`Res: ${res}`))
+//=> 'YAY'
+
+// Handle promises
+Future.fromPromise(fetch('https://api.awesome.com/catOfTheDay'))
+  .fork(
+    err => log('There was an error fetching the cat of the day :('),
+    cat => log('Cat of the day: ' + cat))
+//=> 'Cat of the day: Garfield'
+                        
+//NO ifs
+(foo) => {
+  if (foo) {
+    return bar
+  }
+  return baz
+}
+
+// good
+(foo) => foo ? bar : baz
+// ok
+(foo) => foo && bar || 0
+// good
+(foo) => foo ? bar : { baz: true }
+(foo) => foo ? baz : 0
+                        
+// compose impure depencies
+// bad
+const readFileBody = filePath => {
+  const fileContents = readFileSync(filePath)
+  const json = JSON.parse(fileContents)
+  return json.body
+}
+
+// good
+const getBody = document => document.body
+const readFileBody = compose(getBody, JSON.parse, readFileSync)
+
+// bad
+function fromPairs(pairs) {
+  var result = {}
+  var idx = 0
+  while (idx < pairs.length) {
+    result[pairs[idx][0]] = pairs[idx][1]
+    idx += 1
+  }
+  return result
+}
+
+// good
+const fromPairs = compose(combine, map(([key, value]) => ({ [key]: value })))
+
+// bad
+const splitToKeyValuePair = headerString => {
+  return headerString.split(',')
+    .reduce((result, current) => {
+      const keyValuePair = current.split('=')
+      const key = keyValuePair[0]
+      const value = keyValuePair[1]
+      result[key] = value
+      return result
+    }, {})
+}
+
+// good
+const splitToKeyValuePair = compose(combine, fromPairs, map(split('=')), map(trim), split(','))
+
+
+// statte mutation
+// bad
+const foo = state => {
+  state.count = state.count + 1
+  return state
+}
+
+// good
+const foo = state => object.assign({}, state, { count: state.count + 1 })
+
+// best
+const foo = state => ({ ...state, count: state.count + 1 })
+
+// Use Object.assign to avoid mutating state.
+
+// bad
+const state => {
+  state.id = 1
+  return state
+}
+
+// good
+const state => Object.assign({}, state, { id : 1 }) // native js
+
+const state => ({ ...state, id : 1 }) // es6 syntax
+// Do Not Share State
+
+// bad
+const inc = () => state.count++
+
+// good
+const inc = (state = { count: 0 }) => ({ count : state.count + 1 })
+
+//You should try to avoid mutating local state. 
+//The inconvenience with doing it is that you might inadvertently cause a side effect by mutating a function parameter.
+
+// bad
+const fiftyPercentDiscount = shoe => {
+  shoe.price + shoeVariant.price / 2
+  return shoe
+}
+
+// good
+const fiftyPercentDiscount = shoe => ({ ...shoe, price: shoe.price / 2 })
+
+// Chain http calls
+Future.fromPromise(fetch('https://api.awesome.com/catOfTheDay'))
+  .chain(cat => Future.fromPromise(fetch(`https://api.catfacts.com/${cat}`)))
+  .fork(
+    err => log('There was an error fetching the cat of the day :('),
+    facts => log('Facts for cat of the day: ' + facts))
+//=> 'Facts for cat of the day: Garfield is awesome.'
+  .fork(() => 'can\'t get body of null', b => b)
+
+    
+// recursion
+// recur :: Number -> Number -> Number
+const recur = n => acc =>  n == 0 ? acc : recur(n-1)(n * acc)
+
+// recur :: Number -> Number
+const factorial = (n) => recur(n)(1)
+
+
+// function sigantures hindley-milner
+// functionName :: type -> type -> type
+
+// add :: Number -> Number -> Number
+const add = x => y => x + y
+
+// concat :: [a] -> [a] -> [a]
+const concat = a => b => a.concat(b)
+
+// filter :: (a -> Boolean) -> [a] -> [a]
+const filter = f => a => a.filter(f)
+
+// map :: (a -> b) -> [a] -> [b]
+const map = f => a => a.map(f)
+
+// replace :: String -> String -> String -> String
+const replace = subStr => newSubstr => s => s.replace(subStr, newSubstr)
+
+// uncurriedMaxOfThree :: (Number, Number, Number) -> Number
+uncurriedMaxOfThree (x, y, z) = Math.max(x, y, z)
+
+// curry :: ((a, b) -> c) -> a -> b -> c
+const curry = f => x => y => f(x, y)
+
+// uncurry :: (a -> b -> c) -> (a, b) -> c
+const uncurry => f => (x, y) =>  f(x)(y)
+
+// returnOne :: () -> Number
+const returnOne = () => 1
