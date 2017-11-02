b3a8ffd656db1a7614f3a22c13ecb766b80c3329
sebtn <sebtn@users.noreply.github.com>
2017-11-02 09:29:30 -0400

    Gerrit flow

---
+++ gerrit.js
@@ -0,0 +1,11 @@
+// Gerrit FLOW
+
+git review -d  + idNUmber_from_req // dwonloads a copy for review 
+git review name of branch // will push to gerrit for review
+git --amend // Is the one used to work in acc changes
+
+git fetch // will fecthc last changes to branch
+
+// git fetch and git pull are diff beacuse pulling will also merge
+
+do a change ->>> add ->>> commit(using -m flag onljy) ->>> review ->>> amend ->>> re-review
